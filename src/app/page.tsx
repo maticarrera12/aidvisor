2d12eb0b724c42c683e5be7678376c413904aec3
MatiCarrera <mcarreradev12@gmail.com>
2025-07-03 23:38:40 -0300

    Auth pages, modules layout and logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,23 @@ import { authClient } from "@/lib/auth-client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const {data: session} = authClient.useSession()
   const [email, setEmail] = useState('')
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
-  const onSubmit = ()=>{
-    authClient.signUp.email({
-      email,
-      name,
-      password
-    },{
-       onError: () =>{
-        window.alert('Something went wrong')
-       },
-       onSuccess: ()=>{
-        window.alert("Success")
-       }
+  const router = useRouter()
+  const onSignOut = () => {
+    authClient.signOut({
+      onError: () => {
+        window.alert('Error al cerrar sesión')
+      },
+      onSuccess: () => {
+        window.alert('Sesión cerrada')
+        router.push('/sign-in')
+      }
     })
   }
 
@@ -28,21 +27,15 @@ export default function Home() {
     return(
       <div className="flex flex-col p-4 gap-y-4">
         <p>Logged in as {session.user.name}</p>
-        <Button onClick={()=> authClient.signOut()}>
+        <Button onClick={onSignOut}>
           Sign Out
         </Button>
       </div>
     )
   }
   return (
-<div className="p-4 flex flex-col gap-y-4">
-  <Input type="text" placeholder="name" value={name} onChange={(e)=> setName(e.target.value)}/>
-  <Input type="email" placeholder="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
-  <Input type="password" placeholder="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
-
-  <Button onClick={onSubmit}>
-    Create User
-  </Button>
-</div>
+    <div className="p-4 flex flex-col gap-y-4">
+      {/* Aquí puedes poner tu formulario de registro o login */}
+    </div>
   );
 }
